8bdb88d76f0da250ed37eff5f248c9b3bf74462c
Andy Duff <andyduffdj25@gmail.com>
2026-02-10 19:14:52 -0300

    add png in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,9 +42,13 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <a href="#" className="text-2xl font-bold gradient-text">
-            AV
-          </a>
+          <div >
+                  <img
+                    src="/images/id3D.gif"
+                    alt="Andrés Vallarino"
+                    style={{borderRadius: '50%', objectFit: 'cover', width: 50, height: 50}}
+                  />
+                </div>
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex items-center space-x-8">
