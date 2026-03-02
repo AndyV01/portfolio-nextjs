e6dc79fcc32272aee7708235f2ac64d8230188fe
Andy Duff <andyduffdj25@gmail.com>
2026-03-02 15:50:25 -0300

    feat: add nnew cv

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -47,7 +47,7 @@ const Hero = () => {
                 Contáctame
               </a>
               <a
-                href="/Andres_Vallarino_CV-2026.pdf"
+                href="/Cv_Andres-Vallarino.pdf"
                 download
                 className="px-8 py-3 glass-effect hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg font-medium transition-colors flex items-center gap-2"
               >
